e65680cec5e4dd2afbe81aa0ee53ed405e0bbd24
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-02-22 21:49:56 +0530

    starting with sass

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,19 +5,19 @@ import logoWhite from "../assets/logo-white.png";
 const Hero = () => {
   return (
     <header className="header">
-      <div className="logo-container">
-        <img src={logoWhite} alt="natours logo" className="logo" />
+      <div className="header__logo-container">
+        <img src={logoWhite} alt="natours logo" className="header__logo" />
       </div>
 
-      <main className="main-box">
+      <main className="header__main-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-secondary">
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--secondary">
             is where life happens
           </span>
         </h1>
-        <a href="#" className="btn btn-primary">
-          Check our tours
+        <a href="#" className="btn btn--primary btn--animation">
+          Explore our tours
         </a>
       </main>
     </header>
